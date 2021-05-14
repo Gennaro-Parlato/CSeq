3bd479beb2bfc09e1de0a80f9522d3daf0c496ab
salvatore-latorre <slatorre@unisa.it>
2021-05-14 11:36:20 +0200

    1. removed module dr_workarounds 2. moved the transformation by dr_workarounds to dr_preinstrumenter plus:    introduced the conversion from "__VERIFIER_assert" to "__CPROVER_assume"

--- VeriSmart/modules/dr_preinstrumenter.py
+++ VeriSmart/modules/dr_preinstrumenter.py
@@ -1,17 +1,31 @@
 """ Extends preinstrumenter.py
     written and maintained by CSeq team
 
+    In addition to preinstrumenter.py this module:
+      1. transforms asserts into assumes:
+        a.  assert --> __CPROVER_assume
+        b. __CPROVER_assert --> __CPROVER_assume
+        c. __VERIFIER_assert --> __CPROVER_assume
 
-    This module overrules a transformation of preinstrumenter.py, namely
+      2. overrules a transformation by preinstrumenter.py, namely
        it removes all ERROR labels as follows:
             - goto ERROR; ---> __CSEQ_assume(0);
             - ERROR :;    ----> __CSEQ_assume(0);
-    (preinstrumenter.py was changing them to __CSEQ_assert)
+    (preinstrumenter.py changes them to __CSEQ_assert)
 """
 from modules import preinstrumenter
 #import pycparser.c_parser, pycparser.c_ast, pycparser.c_generator
 
 class dr_preinstrumenter(preinstrumenter.preinstrumenter):
+
+    def visit_FuncCall(self, n):
+        fref = self._parenthesize_unless_simple(n.name)
+
+        if (fref.startswith("assert") or fref.startswith("__CPROVER_assert") or fref.startswith("__VERIFIER_assert")):
+            return '__CPROVER_assume' + '(' + self.visit(n.args) + ')'
+
+        return super(self.__class__, self).visit_FuncCall(n)
+
 
     def visit_Goto(self, n):
         if n.name == self.getErrorLabel():
