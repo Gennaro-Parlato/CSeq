705babeef68a80cdbddcfbfca0c9f8adb515472a
Giulio Garbi <giulio.garbi@gmail.com>
2022-09-16 10:12:59 +0200

    Fix for spurious wakeups

--- VeriSmart/modules/lazyseqBnewschedule.c
+++ VeriSmart/modules/lazyseqBnewschedule.c
@@ -100,7 +100,6 @@ int __cs_cond_wait_1(__cs_cond_t *__cs_cond_to_wait_for, __cs_mutex_t *__cs_m, u
 }
 
 int __cs_cond_wait_2(__cs_cond_t *__cs_cond_to_wait_for, __cs_mutex_t *__cs_m, unsigned int __cs_thread_index) {
-	__CSEQ_assume(*__cs_cond_to_wait_for == 1);
 	__cs_mutex_lock(__cs_m, __cs_thread_index);
 	return 0;
 }
